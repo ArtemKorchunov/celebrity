538743bd02a3550bf7ffbe7c2114f90566ee1ce8
quantum <konstantin.krois@gmail.com>
2018-11-02 21:43:56 +0200

    fix server delete interval

--- server/index.js
+++ server/index.js
@@ -5,29 +5,31 @@ const path = require("path");
 const app = express();
 const uploadsDir = path.join(__dirname, "..", "dist", "img");
 app.use(express.static(path.join(__dirname, "..", "dist")));
-fs.readdir(uploadsDir, function(err, files) {
-  console.log(files);
 
-  files.forEach(function(file, index) {
-    fs.stat(path.join(uploadsDir, file), function(err, stat) {
-      var endTime, now;
-      if (err) {
-        return console.error(err);
-      }
+setInterval(() => 
+  fs.readdir(uploadsDir, function(err, files) {
+    console.log(files);
 
-      now = new Date().getTime();
-      endTime = new Date(stat.ctime).getTime() + 1 * 1000;
-      if (now > endTime) {
-        return rimraf(path.join(uploadsDir, file), function(err) {
-          if (err) {
-            return console.error(err);
-          }
-          console.log("successfully deleted");
-        });
-      }
+    files.forEach(function(file, index) {
+      fs.stat(path.join(uploadsDir, file), function(err, stat) {
+        var endTime, now;
+        if (err) {
+          return console.error(err);
+        }
+
+        now = new Date().getTime();
+        endTime = new Date(stat.ctime).getTime() + 1 * 1000;
+        if (now > endTime) {
+          return rimraf(path.join(uploadsDir, file), function(err) {
+            if (err) {
+              return console.error(err);
+            }
+            console.log("successfully deleted");
+          });
+        }
+      });
     });
-  });
-});
+}), 300 * 1000);
 
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
